7dc88c47971bd4679a177fd5842ba578aaeb5571
Nirav9603 <niravmagatarapara111@gmail.com>
2022-06-09 07:59:31 +0530

    pr4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Loading from './container/Loading';
-import Home from './container/Home';
-
-const HomeWithLoading = Loading(Home);
 
 function App(props) {
   return (
