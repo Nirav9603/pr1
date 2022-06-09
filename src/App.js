956835c57359bac215ea065ee1639a6435270e20
Nirav9603 <niravmagatarapara111@gmail.com>
2022-06-09 07:59:07 +0530

    pr4

--- src/App.js
+++ src/App.js
@@ -5,26 +5,9 @@ import Home from './container/Home';
 const HomeWithLoading = Loading(Home);
 
 function App(props) {
-
-  const [loading,setLoading] = useState(false);
-  const [data,setData] = useState([]);
-
-  const orgData = [
-    {id:101,name:'nirav'},
-    {id:102,name:'kishan'}
-    
-  ];
-
-  useEffect(() =>{
-    setLoading(true)
-    setTimeout(()=>{setLoading(false); setData(orgData)}, 2000)
-  },[])
   return (
     <div>
-        <HomeWithLoading
-          isLoading={loading}
-          isData={data}
-        />
+        
   </div>
   );
 }
