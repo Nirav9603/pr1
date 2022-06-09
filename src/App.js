f45d807f1920e7f7e5822fd74ecd545d68eba635
Nirav9603 <niravmagatarapara111@gmail.com>
2022-06-09 07:54:21 +0530

    pr2(2)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ComponentLifecycle from './container/ComponentLifecycle';
-import Time from './container/Time';
-import TimeFun from './container/TimeFun';
+import React, { useEffect, useState } from 'react';
+import Loading from './container/Loading';
+import Home from './container/Home';
+
+const HomeWithLoading = Loading(Home);
 
 function App(props) {
+
+  const [loading,setLoading] = useState(false);
+  const [data,setData] = useState([]);
+
+  const orgData = [
+    {id:101,name:'nirav'},
+    {id:102,name:'kishan'}
+    
+  ];
+
+  useEffect(() =>{
+    setLoading(true)
+    setTimeout(()=>{setLoading(false); setData(orgData)}, 2000)
+  },[])
   return (
     <div>
-        {/* <ComponentLifecycle /> */}
-        {/* <Time /> */}
-        <TimeFun />
+        <HomeWithLoading
+          isLoading={loading}
+          isData={data}
+        />
   </div>
   );
 }
---
+++ src/container/Loading.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Loading(Component) {
+    
+    return function Load({isData,isLoading}){
+        if(isLoading){
+            return(
+                <h1>Loading...</h1>
+            )
+        }else{
+            return(
+                <Component Data={isData}/>
+            )
+        }
+    }
+}
+
+export default Loading;
