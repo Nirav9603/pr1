f9bdb57a682f3a14f7ec5eac4061d86c8ca449cd
Nirav9603 <niravmagatarapara111@gmail.com>
2022-06-14 11:37:26 +0530

    exam

--- src/App.js
+++ src/App.js
@@ -2,52 +2,78 @@ import React, { useEffect, useState } from 'react';
 import Home from './container/Home';
 import Loading from './container/Loading';
 
-const homeWithLoading = Loading(Home)
+const HomeWithLoading = Loading(Home);
 
 function App(props) {
 
-  const [loading, setLoading] = useState(false)
-  const [data, setData] = useState([])
+  const [loading,setLoading] = useState(false);
+  const [data,setData] = useState([]);
 
-  const employeeData = [
+  const EmployeeData =
+
+[
+
     {
-      id: 101,
-      name: 'Amit',
-      joining_date: "01-06-2021",
-      salary: 50000
+
+        id: 101,
+
+        name: 'Amit',
+
+        joining_date: "01-06-2021",
+
+        salary: 50000
+
     },
+
     {
-      id: 102,
-      name: 'Piyush',
-      joining_date: "01-01-2019",
-      salary: 60000
+
+        id: 102,
+
+        name: 'Piyush',
+
+        joining_date: "01-01-2019",
+
+        salary: 60000
+
     },
+
     {
-      id: 103,
-      name: 'Meet',
-      joining_date: "01-03-2020",
-      salary: 25000
+
+        id: 103,
+
+        name: 'Meet',
+
+        joining_date: "01-03-2020",
+
+        salary: 25000
+
     },
+
     {
-      id: 104,
-      name: 'Lalit',
-      joining_date: "01-12-2021",
-      salary: 30000
+
+        id: 104,
+
+        name: 'Lalit',
+
+        joining_date: "01-12-2021",
+
+        salary: 30000
+
     }
-  ]
+
+];
 
   useEffect(() =>{
     setLoading(true)
-    setTimeout(()=>{setLoading(false); setData(employeeData)}, 2000)
+    setTimeout(()=>{setLoading(false); setData(EmployeeData)}, 2000)
   },[])
-
   return (
     <div>
-      <homeWithLoading 
-        isLoading={loading}
-        isData={data}
-      />
-    </div>
+        <HomeWithLoading
+          isLoading={loading}
+          isData={data}
+        />
+  </div>
   );
 }
 
