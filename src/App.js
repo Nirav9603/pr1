6a8bcf282ccfcdcb8b134141ac344b6c24db0fa5
Nirav9603 <niravmagatarapara111@gmail.com>
2022-06-14 11:19:17 +0530

    exam

--- src/App.js
+++ src/App.js
@@ -2,10 +2,45 @@ import React, { useEffect, useState } from 'react';
 import Counter from './container/Counter';
 
 function App(props) {
+
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([])
+
+  employeeData = [
+    {
+      id: 101,
+      name: 'Amit',
+      joining_date: "01-06-2021",
+      salary: 50000
+    },
+    {
+      id: 102,
+      name: 'Piyush',
+      joining_date: "01-01-2019",
+      salary: 60000
+    },
+    {
+      id: 103,
+      name: 'Meet',
+      joining_date: "01-03-2020",
+      salary: 25000
+    },
+    {
+      id: 104,
+      name: 'Lalit',
+      joining_date: "01-12-2021",
+      salary: 30000
+    }
+  ]
+
+  useEffect(()=>(
+    setLoading(true)
+    setTimeout({})
+  ),[])
   return (
     <div>
-        <Counter />
-  </div>
+
+    </div>
   );
 }
 
---
+++ src/container/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loading(Component) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Loading;
