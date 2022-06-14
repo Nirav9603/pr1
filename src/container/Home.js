72e3076c5abed5bc0e2a18278655bbdaa62aaf99
Nirav9603 <niravmagatarapara111@gmail.com>
2022-06-14 11:32:35 +0530

    exam

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 function Home({Data}) {
+    
     return (
         <div>
-            
+            {
+                Data.map((d, i)=>{
+                    return(
+                        <div key={i}>
+                            <h3>{d.id}</h3>
+                            <h2>{d.name}</h2>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 }
